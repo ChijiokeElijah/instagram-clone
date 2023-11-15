dbbd8b3c6183c63f81fbd8337cefc6635fc31a69
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2023-11-15 20:41:15 +0100

    implement sticky header

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ import {HomeIcon} from "@heroicons/react/solid"
 
 export default function Header() {
   return (
+        <div className='shadow-sm border-b sticky top-0 bg-white z-30'>
           <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
           {/* Left */}
             <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
@@ -37,5 +38,7 @@ export default function Header() {
           <img src="https://th.bing.com/th/id/OIP.qNBaBUh5A4nnZgbCWEHLdQAAAA?w=120&h=180&c=7&r=0&o=5&pid=1.7" alt='user-image' className='h-10 w-10 rounded-full' />
         </div>
         </div>
-  )
+
+        </div>
+            )
 }
