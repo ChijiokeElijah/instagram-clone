d87b0eafc94d09ef830b20a3ec52c51508d39982
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2023-11-18 11:58:44 +0100

    create the header and image sections of tge post component

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,16 @@
-
+import { DotsHorizontalIcon } from "@heroicons/react/solid"
 
 export default function Post({img, userImg, caption, username, id}) {
   return (
-    <div>
-        <h1>{username}</h1>
+    <div className="bg-white my-7 border  rounded-md">
+        {/* Post Header */}
+        <div className="flex items-center p-5">
+            <img className="h-12 w-12 rounded-full object-cover border p-1 mr-3" src={userImg} alt={username}/>
+            <p className="font-bold flex-1">{username}</p>
+            <DotsHorizontalIcon className="h-5"/>
+        </div>
+        {/* Post image */}
+        <img className="object-cover w-full " src={img} alt=""/>
 
     </div>
   )
